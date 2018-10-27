9dca953da1dd336f702b455c20688c9644fb1f57
AstiaSun <nastiasunrise@gmail.com>
2018-10-27 05:45:57 +0300

    update db connection to mongo and add simple requests for filer

--- src/app.py
+++ src/app.py
@@ -1,14 +1,25 @@
 from flask import Flask, render_template
 
+from db_pool.db import DBPool
+
 # start application
+
 app = Flask(__name__)
+db = DBPool('192.168.163.132', 27017)
+db.connect()
 
 
 # logger = logging.basicConfig(level=logging.DEBUG)
 
 
-@app.route('/hello')
-def hello_world():
+@app.route('/', methods=['GET'])
+def get_filtering_params():
+    knowledge_areas = db.get_knowledge_areas()
+    regions = db.get_regions()
+    university_titles = db.get_university_titles()
+    print(knowledge_areas)
+    print(regions)
+    print(university_titles)
     return render_template('hello.html')
 
 
--- src/db_pool/db.py
+++ src/db_pool/db.py
@@ -1,2 +1,21 @@
 from pymongo import MongoClient
-client = MongoClient('192.168.163.132', 27017)
+
+
+class DBPool(object):
+    def __init__(self, host, port):
+        self._host = host
+        self._port = port
+        self._db = None
+
+    def connect(self):
+        client = MongoClient(self._host, self._port)
+        self._db = client['test']
+
+    def get_university_titles(self):
+        return self._db.universities.find({}, {'univ_title': 1})
+
+    def get_knowledge_areas(self):
+        return self._db.areas.distinct('area_type')
+
+    def get_regions(self):
+        self._db.universities.distinct('univ_location')
